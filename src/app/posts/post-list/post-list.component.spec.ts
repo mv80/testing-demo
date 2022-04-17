2c7338d2e6f124b129b941b3c37c1e227b998955
meital.valenci <mvalenci@varonis.com>
2022-04-17 09:10:02 +0300

    refactor

--- src/app/posts/post-list/post-list.component.spec.ts
+++ src/app/posts/post-list/post-list.component.spec.ts
@@ -5,6 +5,7 @@ import {
   TestBed
 } from '@angular/core/testing';
 import { of } from 'rxjs';
+import { PostComponent } from '../post/post.component';
 import { IPost } from '../post/post.model';
 import { PostService } from '../services/post.service';
 import { PostListComponent } from './post-list.component';
@@ -38,7 +39,7 @@ describe('PostListComponent', () => {
   ];
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [PostListComponent],
+      declarations: [PostListComponent, PostComponent],
       providers: [{ provide: PostService, useFactory: () => postServiceMock }],
     }).compileComponents();
   }));
@@ -57,6 +58,7 @@ describe('PostListComponent', () => {
   it('should set posts to an array of posts', () => {
     expect(component.posts.length).toBeGreaterThan(0);
   });
+  
   describe('update likes', () => {
     const mockPostId = 1;
     beforeEach(() => {
@@ -71,7 +73,6 @@ describe('PostListComponent', () => {
     it('should increase number of likes by one once updateLikes is called', fakeAsync(() => {
       const numOfPostLikesBeforeUpdate = component.posts[0].likes;
       component.updateLikes(1).then(() => {
-
         const numOfPostLikeAfterUpdate = component.posts[0].likes;
         expect(numOfPostLikeAfterUpdate).toBeGreaterThan(
           numOfPostLikesBeforeUpdate
@@ -94,15 +95,6 @@ describe('PostListComponent', () => {
       await component.updateLikes(mockPostId);
       const post = component.posts.find((post) => post.id === mockPostId);
       expect(post.isLiked).toBeTrue();
-    });
-
-    // same test  - using done callback
-    it('should set post is liked to true after call for updatePostLikes', (done) => {
-      component.updateLikes(1).then(() => {
-        const post = component.posts.find((post) => post.id === 1);
-        expect(post.isLiked).toBeTrue();
-        done();
-      });
     });
   });
 });
